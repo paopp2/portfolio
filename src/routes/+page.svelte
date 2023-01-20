4487da4ac2d0ef306023bd5bee6127f77acfe7e1
Paolo Pepito <npaolopepito@gmail.com>
2023-01-20 10:25:29 +0800

    Temp layout and text for HeroSection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import AppBar from "./AppBar.svelte";
+	import HeroSection from "./HeroSection.svelte";
+</script>
+
+<div>
+  <AppBar/>
+  <main class="absolute top-0">
+    <HeroSection/>
+    <HeroSection/>
+    <HeroSection/>
+  </main>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: #0b1426;
+  }
+</style>
---
+++ src/routes/HeroSection.svelte
@@ -0,0 +1,13 @@
+<div class="px-32 text-white font-bold flex items-center h-screen">
+	<div class="">
+		<p>Hi! My name is</p>
+		<h1 class="text-9xl">Redford White</h1>
+		<p>Lorem ipsum blah blah the quick brown fox jumps over the lazy dog</p>
+	</div>
+</div>
+
+<style>
+	h1 {
+		color: #ced6f5;
+	}
+</style>

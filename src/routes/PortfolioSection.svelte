5e8813069c1573665fc165f897ea304a6bce1cf3
Paolo Pepito <npaolopepito@gmail.com>
2023-03-08 20:35:58 +0800

    Update placeholder layout for PortfolioSection

--- src/routes/PortfolioSection.svelte
+++ src/routes/PortfolioSection.svelte
@@ -1,3 +1,15 @@
-<div class="px-64 text-white font-bold flex items-center h-screen">
-	<div>Portfolio Section</div>
+<div class="flex flex-col items-center justify-center h-screen">
+  <h1 class="text-5xl text-[#ced6f5] font-bold">Some of my works</h1>
+  <div class="flex flex-row items-center">
+	<div class="flex-1 flex-col text-center text-white">
+		<h1>Random text 1</h1>
+		<h1>Random text 2</h1>
+	</div>
+	<div class="flex-1 flex-col text-center">
+		<img 
+			class="w-1/2 mx-auto"
+			src="https://cdn.freebiesupply.com/logos/large/2x/random-logo-png-transparent.png" 
+			alt="some random logo">
+	</div>
+  </div>
 </div>

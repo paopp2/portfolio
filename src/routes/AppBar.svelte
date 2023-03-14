5ca18611aea1c9650695b4cbe8f70bb95206225d
Paolo Pepito <npaolopepito@gmail.com>
2023-03-14 23:34:54 +0800

    Autoformat AppBar

--- src/routes/AppBar.svelte
+++ src/routes/AppBar.svelte
@@ -1,36 +1,34 @@
 <script lang="ts">
-	let navBarLinks: string[] = [ 'Home', 'Portfolio', 'About', 'Contact' ];
+	let navBarLinks: string[] = ['Home', 'Portfolio', 'About', 'Contact'];
 	let currentNavLink = navBarLinks[0];
-	
+
 	$: getNavBarLinkClass = (navLink: string) => {
-		return (currentNavLink === navLink)
+		return currentNavLink === navLink
 			? 'block py-2 pl-3 pr-4 text-white bg-[#7de1c5] rounded md:bg-transparent md:text-[#7de1c5] md:p-0 dark:text-white'
-			: 'block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-	}
+			: 'block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
+	};
 </script>
 
 <nav class="py-9">
-	<div class="container flex flex-wrap items-center justify-between mx-auto">
+	<div class="container mx-auto flex flex-wrap items-center justify-between">
 		<a href="https://flowbite.com/" class="flex items-center">
 			<img
 				src="https://flowbite.com/docs/images/logo.svg"
-				class="h-6 mr-3 sm:h-9"
-				alt="Flowbite Logo"
-			/>
-			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-				>Paolo Pepito</span
-			>
+				class="mr-3 h-6 sm:h-9"
+				alt="Flowbite Logo" />
+			<span
+				class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>Paolo Pepito</span>
 		</a>
 		<button
 			data-collapse-toggle="navbar-default"
 			type="button"
-			class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+			class="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
 			aria-controls="navbar-default"
-			aria-expanded="false"
-		>
+			aria-expanded="false">
 			<span class="sr-only">Open main menu</span>
 			<svg
-				class="w-6 h-6"
+				class="h-6 w-6"
 				aria-hidden="true"
 				fill="currentColor"
 				viewBox="0 0 20 20"
@@ -38,9 +36,7 @@
 				><path
 					fill-rule="evenodd"
 					d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-					clip-rule="evenodd"
-				/></svg
-			>
+					clip-rule="evenodd" /></svg>
 		</button>
 		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
 			<ul class="flex flex-row space-x-8">
@@ -48,11 +44,11 @@
 					<li>
 						<a
 							href="#trial"
-							class="{getNavBarLinkClass(navLink)}"
+							class={getNavBarLinkClass(navLink)}
 							on:click={() => (currentNavLink = navLink)}
-							aria-current="page">{navLink}</a
-						>
+							aria-current="page">{navLink}</a>
 					</li>
+					""
 				{/each}
 			</ul>
 		</div>

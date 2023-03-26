5ddc56f7cd13aaeb918eb0185e2ba7c57288f01f
Paolo Pepito <npaolopepito@gmail.com>
2023-03-27 00:52:04 +0800

    Modify section divider design

--- src/routes/PortfolioSection.svelte
+++ src/routes/PortfolioSection.svelte
@@ -1,5 +1,8 @@
-<section id="portfolio-section">
-	<div class="flex h-screen flex-col items-center justify-center">
+<section class="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-900">
+	<div class="flex bg-[#0b1426] right-0 left-0 rounded-b-[100%] h-32" />
+	<div
+		class="flex flex-col items-center justify-center h-screen"
+		id="portfolio-section">
 		<h1 class="text-5xl font-bold text-[color:var(--text-color)]">
 			Some of my works
 		</h1>

4148825397c0d7831f001270efe249fe1b1e6693
Paolo Pepito <npaolopepito@gmail.com>
2023-04-09 23:09:49 +0800

    Move ProjectBlock to a shared component

---
+++ src/lib/components/ProjectBlock.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let name: string;
+	export let description: string;
+	export let imageSrc: string;
+	export let textFirst: boolean = true;
+</script>
+
+<div
+	class="flex rounded-[35px] bg-[#111f3b] shadow-2xl w-3/5 justify-center mb-20">
+	{#if textFirst}
+		<div class="text-half">
+			<h1>{name}</h1>
+			<p>{description}</p>
+		</div>
+
+		<img class="img-half" src={imageSrc} alt="random_alt" />
+	{:else}
+		<img class="img-half" src={imageSrc} alt="random_alt" />
+
+		<div class="text-half">
+			<h1>{name}</h1>
+			<p>{description}</p>
+		</div>
+	{/if}
+</div>
+
+<style>
+	.text-half {
+		@apply w-1/2 p-16;
+	}
+
+	.text-half > h1 {
+		@apply mb-2 text-5xl font-bold leading-tight text-[var(--text-color)] pb-8;
+	}
+
+	.text-half > p {
+		@apply mb-4 text-neutral-600;
+	}
+
+	.img-half {
+		@apply rounded-[35px] object-cover w-1/2 p-3;
+	}
+</style>
--- src/routes/PortfolioSection.svelte
+++ src/routes/PortfolioSection.svelte
@@ -1,68 +1,56 @@
-<script>
+<script lang="ts">
 	import SectionHeader from '$lib/components/SectionHeader.svelte';
+	import ProjectBlock from '$lib/components/ProjectBlock.svelte';
+
+	type Project = {
+		name: string;
+		description: string;
+		imageSrc: string;
+		textFirst: boolean;
+	};
+	const myProjects: Project[] = [
+		{
+			name: 'Kasado',
+			description:
+				'Some text about Kasado. Some text about Kasado.' +
+				'Some text about Kasado. Some text about Kasado. ' +
+				'Some text about Kasado. Kasado. Some text about Kasado.' +
+				'Some text about Kasado.',
+			imageSrc:
+				'https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top',
+			textFirst: true
+		},
+		{
+			name: 'Kapiot',
+			description:
+				'Lorem Ipsum is simply dummy text of the printing and' +
+				"typesetting industry. Lorem Ipsum has been the industry's " +
+				'standard dummy text ever since the 1500s, when an unknown ' +
+				'printer took a galley of type and scrambled it to make ',
+			imageSrc:
+				'https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top',
+			textFirst: false
+		},
+		{
+			name: 'Marnikko',
+			description:
+				'Some text about Marnikko. Some text about Marnikko.' +
+				'Some text about Marnikko. Some text about Marnikko. ' +
+				'Some text about Marnikko. Some text about Marnikko.' +
+				'Some text about Marnikko.',
+			imageSrc:
+				'https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top',
+			textFirst: true
+		}
+	];
 </script>
 
 <section id="portfolio-section" class="min-h-screen">
 	<SectionHeader sectionTitle={'Some of my Work'} />
 
 	<div class="flex flex-col items-center justify-center">
-		<div
-			class="flex rounded-[35px] bg-[#111f3b] shadow-2xl w-3/5 justify-center mb-20">
-			<div class="w-1/2 p-16">
-				<h1
-					class="mb-2 text-5xl font-bold leading-tight text-[color:var(--text-color)] pb-8">
-					Kasado
-				</h1>
-				<p class="mb-4 text-neutral-600">
-					Some text about Kasado. Some text about Kasado. Some text about
-					Kasado. Some text about Kasado. Some text about Kasado. Kasado. Some
-					text about Kasado. Some text about Kasado.
-				</p>
-			</div>
-
-			<img
-				class="rounded-[35px] object-cover w-1/2 p-3"
-				src="https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top"
-				alt="hello" />
-		</div>
-
-		<div
-			class="flex rounded-[35px] bg-[#111f3b] shadow-2xl w-3/5 justify-center mb-20">
-			<img
-				class="rounded-[35px] object-cover w-1/2 p-3"
-				src="https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top"
-				alt="hello" />
-			<div class="w-1/2 p-16">
-				<h1
-					class="mb-2 text-5xl font-bold leading-tight text-[color:var(--text-color)] pb-8">
-					Kapiot
-				</h1>
-				<p class="mb-4 text-neutral-600 ">
-					Some text about Kapiot. Some text about Kapiot. Some text about
-					Kapiot. Some text about Kapiot. Some text about Kapiot. Some text
-					about Kapiot.
-				</p>
-			</div>
-		</div>
-
-		<div
-			class="flex rounded-[35px] bg-[#111f3b] shadow-2xl w-3/5 justify-center mb-20">
-			<div class="w-1/2 p-16">
-				<h1
-					class="mb-2 text-5xl font-bold leading-tight text-[color:var(--text-color)] pb-8">
-					Kasado
-				</h1>
-				<p class="mb-4 text-neutral-600">
-					Some text about Kasado. Some text about Kasado. Some text about
-					Kasado. Some text about Kasado. Some text about Kasado. Kasado. Some
-					text about Kasado. Some text about Kasado.
-				</p>
-			</div>
-
-			<img
-				class="rounded-[35px] object-cover w-1/2 p-3"
-				src="https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top"
-				alt="hello" />
-		</div>
+		{#each myProjects as project}
+			<ProjectBlock {...project} />
+		{/each}
 	</div>
 </section>

13f19282dd875b33b414c4bb6d31ff9efd6f7502
Paolo Pepito <npaolopepito@gmail.com>
2023-04-19 20:02:49 +0800

    Move text content at PortfolioSection to site-content.json

--- src/routes/PortfolioSection.svelte
+++ src/routes/PortfolioSection.svelte
@@ -1,48 +1,12 @@
 <script lang="ts">
 	import SectionHeader from '$lib/components/SectionHeader.svelte';
 	import ProjectBlock from '$lib/components/ProjectBlock.svelte';
+	import siteContent from '$lib/assets/site-content.json';
 
-	type Project = {
-		name: string;
-		description: string;
-		imageSrc: string;
-		textFirst: boolean;
-	};
-	const myProjects: Project[] = [
-		{
-			name: 'Kasado',
-			description:
-				'Some text about Kasado. Some text about Kasado.' +
-				'Some text about Kasado. Some text about Kasado. ' +
-				'Some text about Kasado. Kasado. Some text about Kasado.' +
-				'Some text about Kasado.',
-			imageSrc:
-				'https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top',
-			textFirst: true
-		},
-		{
-			name: 'Kapiot',
-			description:
-				'Lorem Ipsum is simply dummy text of the printing and' +
-				"typesetting industry. Lorem Ipsum has been the industry's " +
-				'standard dummy text ever since the 1500s, when an unknown ' +
-				'printer took a galley of type and scrambled it to make ',
-			imageSrc:
-				'https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top',
-			textFirst: false
-		},
-		{
-			name: 'Marnikko',
-			description:
-				'Some text about Marnikko. Some text about Marnikko.' +
-				'Some text about Marnikko. Some text about Marnikko. ' +
-				'Some text about Marnikko. Some text about Marnikko.' +
-				'Some text about Marnikko.',
-			imageSrc:
-				'https://cdn.dribbble.com/users/4001713/screenshots/14872385/media/7dd9babdcdfe199b97a060991f4c1a08.png?compress=1&resize=400x300&vertical=top',
-			textFirst: true
-		}
-	];
+	const myProjects = siteContent.portfolio.projects.map((projObj, i) => ({
+		...projObj,
+		textFirst: i % 2 === 1 // Alternate text first layout
+	}));
 </script>
 
 <section id="portfolio-section" class="min-h-screen">

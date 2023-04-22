a7deca1e31d21dbc944cf302859b46f46e0bc5a8
Paolo Pepito <npaolopepito@gmail.com>
2023-04-22 14:05:54 +0800

    Minor change in wording for PortfolioSec header

--- src/routes/PortfolioSection.svelte
+++ src/routes/PortfolioSection.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <section id="portfolio-section" class="min-h-screen">
-	<SectionHeader sectionTitle={'Some of my Work'} />
+	<SectionHeader sectionTitle={'Some of my work'} />
 
 	<div class="flex flex-col items-center justify-center">
 		{#each myProjects as project}

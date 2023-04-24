adb8f3e639694399feb0497397db1945e05d119d
Paolo Pepito <npaolopepito@gmail.com>
2023-04-25 01:25:22 +0800

    Fix x margin for SectionHeader

--- src/lib/components/SectionHeader.svelte
+++ src/lib/components/SectionHeader.svelte
@@ -2,8 +2,8 @@
 	export let sectionTitle: string;
 </script>
 
-<div class="relative flex items-center px-[310px] mb-8 pt-8">
+<div class="relative flex items-center px-[310px] mx-8 mb-8 pt-8">
 	<div class="w-1/12 border-t-2 border-[#111f3b]" />
 	<span class="flex-shrink text-slate-600 text-4xl mx-8">{sectionTitle}</span>
-	<div class="flex-grow border-t-2 border-[#111f3b] mr-8" />
+	<div class="flex-grow border-t-2 border-[#111f3b]" />
 </div>

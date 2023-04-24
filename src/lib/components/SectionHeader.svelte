032e603277b9e848094275e184c1f1c389b32eda
Paolo Pepito <npaolopepito@gmail.com>
2023-04-24 13:57:17 +0800

    Remove vertical padding for section headers

--- src/lib/components/SectionHeader.svelte
+++ src/lib/components/SectionHeader.svelte
@@ -2,7 +2,7 @@
 	export let sectionTitle: string;
 </script>
 
-<div class="relative flex py-14 items-center px-[310px] mb-8">
+<div class="relative flex items-center px-[310px] mb-8">
 	<div class="w-1/12 border-t-2 border-[#111f3b]" />
 	<span class="flex-shrink text-slate-600 text-4xl mx-8">{sectionTitle}</span>
 	<div class="flex-grow border-t-2 border-[#111f3b] mr-8" />

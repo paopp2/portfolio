f2cd5e87f16fa7528cc9a83f715e70ac0d82b8c5
Paolo Pepito <npaolopepito@gmail.com>
2023-05-15 05:15:38 +0800

    Add project screenshots

--- src/lib/components/ProjectBlock.svelte
+++ src/lib/components/ProjectBlock.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import kasadoSS from '$lib/assets/project_screenshots/kasado.png';
+	import kapiotSS from '$lib/assets/project_screenshots/kapiot.png';
+	import marnikkoSS from '$lib/assets/project_screenshots/marnikko.png';
+
 	export let name: string;
 	export let description: string;
-	export let imageSrc: string;
 	export let textFirst: boolean = true;
+
+	const projectSSMap: { [projectName: string]: string } = {
+		Kasado: kasadoSS,
+		Kapiot: kapiotSS,
+		Marnikko: marnikkoSS
+	};
 </script>
 
 <div
@@ -13,9 +22,9 @@
 			<p>{description}</p>
 		</div>
 
-		<img class="img-half" src={imageSrc} alt="random_alt" />
+		<img class="img-half" src={projectSSMap[name]} alt="random_alt" />
 	{:else}
-		<img class="img-half" src={imageSrc} alt="random_alt" />
+		<img class="img-half" src={projectSSMap[name]} alt="random_alt" />
 
 		<div class="text-half">
 			<h1>{name}</h1>
